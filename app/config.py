8f8b66b04a9ddc67c03d471ba03e026e965a583d
Nick Ross <nick.ir.ross@gmail.com>
2022-10-23 13:29:08 +0100

    feat: add sentry

--- app/config.py
+++ app/config.py
@@ -2,3 +2,4 @@ import os
 
 DATABASE_URL = os.environ.get("DATABASE_URL", "sqlite:///")
 SLACK_WEBHOOK_URL = os.environ.get("SLACK_WEBHOOK_URL")
+SENTRY_DSN = os.environ.get("SENTRY_DSN")
--- app/main.py
+++ app/main.py
@@ -1,21 +1,20 @@
+import sentry_sdk
 from fastapi import Depends, FastAPI
 from fastapi.staticfiles import StaticFiles
 from sqlalchemy.orm import Session
 
-from app import crud, models, schemas
-from app.database import SessionLocal, engine
+from app import config, crud, models, schemas
+from app.database import engine
 from app.notify import send_slack_message
+from app.utils import get_db
+
+
+sentry_sdk.init(
+    dsn=config.SENTRY_DSN,
+    traces_sample_rate=1.0,
+)
 
 models.Base.metadata.create_all(bind=engine)
-
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
 
 api_app = FastAPI(title="API")
 app = FastAPI(title="Main App")
